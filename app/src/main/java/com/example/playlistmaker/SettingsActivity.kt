7c6621d1539b7bd8dceefa42c9276e90ccb6752f
X4PDK0P <viktor@antufyev.ru>
2024-06-21 18:43:22 +0300

    Ready settings activity

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
@@ -18,5 +19,41 @@ class SettingsActivity : AppCompatActivity() {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
         }
+
+
+        val shareBtn = findViewById<com.google.android.material.textview.MaterialTextView>(R.id.share)
+
+        shareBtn.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            val message = "https://practicum.yandex.ru/profile/android-developer-plus/"
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(shareIntent)
+        }
+
+
+        val supportBtn = findViewById<com.google.android.material.textview.MaterialTextView>(R.id.support)
+
+        supportBtn.setOnClickListener {
+            val messageTheme = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("vik.antufev@yandex.ru"))
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, messageTheme)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(shareIntent)
+        }
+
+
+        val docBtn = findViewById<com.google.android.material.textview.MaterialTextView>(R.id.doc)
+
+        docBtn.setOnClickListener {
+            val url = "https://yandex.ru/legal/practicum_offer/"
+            val shareIntent = Intent(Intent.ACTION_VIEW)
+            shareIntent.data = Uri.parse(url)
+            startActivity(shareIntent)
+        }
+
     }
 }
